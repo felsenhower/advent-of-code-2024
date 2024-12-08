a78396b068e4ee4d54e3c489509c5c504e870229
Ruben Felgenhauer <Ruben.Felgenhauer@posteo.de>
2024-12-08 13:10:36 +0100

    Add 8.2

---
+++ 08/day_08-02.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import sys
+import itertools
+
+def main() -> None:
+    with open(sys.argv[1], "r") as f:
+        puzzle = [line.strip() for line in f]
+    height = len(puzzle)
+    width = len(puzzle[0])
+    antenna_positions = set()
+    antenna_positions_by_freq = dict()
+    for (y,line) in enumerate(puzzle):
+        for (x, freq) in enumerate(line):
+            if freq == ".":
+                continue
+            antenna_positions.add((y,x,))
+            if freq not in antenna_positions_by_freq:
+                antenna_positions_by_freq[freq] = []
+            antenna_positions_by_freq[freq].append((y,x,))
+    antinodes = antenna_positions.copy()
+    for (freq, positions) in antenna_positions_by_freq.items():
+        for (first, second) in itertools.permutations(positions, 2):
+            while True:
+                antinode = ((2 * second[0] - first[0]), (2 * second[1] - first[1]))
+                is_in_map = ((antinode[0] >= 0) and (antinode[0] < height) and (antinode[1] >= 0) and (antinode[1] < width))
+                if not is_in_map:
+                    break
+                antinodes.add(antinode)
+                (first, second) = (second, antinode)
+    print(len(antinodes))
+
+if __name__ == '__main__':
+    main()
